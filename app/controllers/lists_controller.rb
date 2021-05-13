36aea8ab5e7a17202605213edc66ec16cdda52fc
Marc Baguet <marcbaguet@hotmail.fr>
2021-05-13 18:53:18 +0200

    watch list day one

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,38 @@
+class ListsController < ApplicationController
+  before_action :set_params, only: [:destroy, :show]
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
