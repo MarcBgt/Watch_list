01b7d030ec08a66edfa7e5719ab9e0dcdde250be
Marc Baguet <marcbaguet@hotmail.fr>
2021-05-13 22:23:20 +0200

    deuxieme push watch list day one

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :list
 
   validates :content, presence: true
-  validates :rating, umericality: { only_integer: true }, inclusion: {in: (0..5)}
+  validates :rating, numericality: { only_integer: true }, inclusion: {in: (0..5)}
 end
